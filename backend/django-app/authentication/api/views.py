c1e7492f777fa910fb2d1f9a30dba69aca1ae858
Leroy Pereira <leroy.pereira@outlook.com>
2024-09-26 22:50:16 +0100

    TASK: Initial commit of the django app

---
+++ backend/django-app/authentication/api/views.py
@@ -0,0 +1,22 @@
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.permissions import IsAuthenticated
+
+from authentication.api.serializers import UserSerializer
+from authentication.models import User
+
+class UserProfileAPIView(RetrieveModelMixin, GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, )
+    queryset = User.objects.all()
+
+    def get_object(self):
+        return self.request.user
+
+    def get(self, request, *args, **kwargs):
+        """
+        User profile
+
+        Get profile of current logged in user.
+        """
+        return self.retrieve(request, *args, **kwargs)
